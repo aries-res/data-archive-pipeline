38fad318b5e08f0cd66a5f0eb5d4e3dc4da4e36e
Ankita Sahu <ankitasahu.aries@gmail.com>
2024-05-07 17:56:06 +0530

    fix:deduplication of header

--- src/generateMasterHeader.py
+++ src/generateMasterHeader.py
@@ -1,6 +1,7 @@
 __author__ = 'ankita sahu'
 __email__ = 'sahuankita2203@gmail.com'
 
+import gc
 import glob
 import os
 import pandas as pd
@@ -25,7 +26,8 @@ class MasterHeader:
 
     def generateMasterHeaderCSV(self, telescope_name=None):
         master_header_filepath = self.current_filepath + "MasterHeaderList_" + telescope_name + ".csv"
-        if not os.path.exists(master_header_filepath):
+        # if not os.path.exists(master_header_filepath):      # for first run
+        if os.path.exists(master_header_filepath):  # for subsequent runs after first run
             master_header = set()
             with open(master_header_filepath, 'w+') as out_file:
                 for extn in fits_file_extension_list:
@@ -34,12 +36,15 @@ class MasterHeader:
                         with fits.open(files) as hdulist:
                             header = hdulist[0].header
                             dict_header = {k: header[k] for k in header.keys() if k}
-                            master_header.update(dict_header)
-                    df = pd.DataFrame(sorted(list(master_header)))
-                    df.to_csv(out_file, sep=',', index=False, header=False, encoding='utf-8')
+                        master_header.update(dict_header)
+                df = pd.DataFrame(sorted(master_header))
+                df.to_csv(out_file, sep=',', index=False, header=False, encoding='utf-8')
+                del df
+        gc.collect()
+
 
 if __name__ == "__main__":
     o_mh = MasterHeader("config.ini")
-    o_mh.readConfigurationFile(mode="TEST")     # to be disabled when run in server
-    # o_mh.readConfigurationFile()      # to be enabled when run in server
+    # o_mh.readConfigurationFile(mode="TEST")     # to be disabled when run in server
+    o_mh.readConfigurationFile()      # to be enabled when run in server
     o_mh.generateMasterHeaderCSV(telescope_name="DOT")
